95a27b7886c0d573d820c15a3d9a475baa60dd3c
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2019-11-26 03:05:32 +0100

    Extract Recipecell to it's own file

---
+++ Recipes/Recipes/RecipesList/RecipeCell.swift
@@ -0,0 +1,153 @@
+//
+//  RecipeCell.swift
+//  Recipes
+//
+//  Created by Hugo Alonso on 26/11/2019.
+//  Copyright © 2019 halonso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import Kingfisher
+
+class RecipeCell: UITableViewCell {
+
+    private lazy var titleLabel: UILabel = self.createTitleLabel()
+    private lazy var ingredients: UILabel = self.createIngredientsLabel()
+    private lazy var hasLactose: UILabel = self.hasLactoseLabel()
+    private lazy var recipeImage: UIImageView = self.createRecipeImageView()
+    private lazy var makeFavorite: UIButton = self.createMakeFavoriteButton()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        backgroundColor = .lightGray
+        selectionStyle = .none
+
+        addSubview(recipeImage)
+        addSubview(makeFavorite)
+        addSubview(titleLabel)
+        addSubview(ingredients)
+        addSubview(hasLactose)
+
+        recipeImage.snp.makeConstraints { (make) in
+            make.width.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.topMargin.equalToSuperview()
+            make.height.equalTo(250)
+        }
+
+        makeFavorite.snp.makeConstraints { make in
+            make.top.equalTo(recipeImage.snp.bottom).offset(8)
+            make.right.equalToSuperview().inset(8)
+            make.width.equalTo(120)
+            make.height.equalTo(50)
+            make.bottom.greaterThanOrEqualToSuperview().offset(8).priority(.low)
+        }
+
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(recipeImage.snp.bottom).offset(8)
+            make.left.equalToSuperview().offset(8)
+            make.height.greaterThanOrEqualTo(44)
+            make.right.equalTo(makeFavorite.snp.left)
+        }
+
+        ingredients.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(8)
+            make.left.equalTo(titleLabel.snp.left)
+            make.right.equalTo(titleLabel.snp.right)
+            make.height.greaterThanOrEqualTo(44)
+            make.bottom.equalToSuperview().inset(8)
+        }
+
+        hasLactose.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(60)
+            make.height.equalTo(44)
+            make.width.equalTo(200)
+        }
+
+        clipsToBounds = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setTitle(_ title: String) {
+        self.titleLabel.text = title
+    }
+
+    func setIngredients(_ ingredients: String) {
+        self.ingredients.text = ingredients
+    }
+
+    func setFavorited(_ favorited: Bool) {
+        let text = favorited ? "Remove favorite" : "Make favorite"
+        makeFavorite.setTitle(text, for: .normal)
+    }
+
+    func setHasLactose(_ hasLactose: Bool) {
+        self.hasLactose.isHidden = !hasLactose
+    }
+
+    func setImage(_ image: URL?) {
+        self.recipeImage.kf.indicatorType = .activity
+        DispatchQueue.main.async {
+            self.recipeImage.kf.setImage(
+                with: image
+            )
+        }
+    }
+}
+
+// MARK: Initialisers
+extension RecipeCell {
+    private func createRecipeImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.accessibilityIdentifier = "recipeImage"
+        imageView.backgroundColor = .clear
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }
+
+    private func createTitleLabel() -> UILabel {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.accessibilityIdentifier = "title"
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        return label
+    }
+
+    private func createIngredientsLabel() -> UILabel {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.accessibilityIdentifier = "ingredients"
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        return label
+    }
+
+    private func hasLactoseLabel() -> UILabel {
+        let label = UILabel()
+        label.backgroundColor = .black
+        label.textColor = .white
+        label.alpha = 0.8
+        label.textAlignment = .center
+        label.text = "Has Lactose"
+        label.accessibilityIdentifier = "lactose"
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 1
+
+        let radians = CGFloat.pi * CGFloat(45) / CGFloat(180.0)
+        label.transform = CGAffineTransform(rotationAngle: radians)
+        return label
+    }
+
+    private func createMakeFavoriteButton() -> UIButton {
+        let favorite = UIButton()
+        return favorite
+    }
+}
--- Recipes/Recipes/RecipesList/RecipesListView.swift
+++ Recipes/Recipes/RecipesList/RecipesListView.swift
@@ -11,7 +11,6 @@ import UIKit
 import SnapKit
 import RxSwift
 import RxCocoa
-import Kingfisher
 
 final class RecipesListView: UIViewController, ViewInterface {
 
@@ -73,6 +72,7 @@ final class RecipesListView: UIViewController, ViewInterface {
             }).disposed(by: disposeBag)
 
         recipes
+            .observeOn(MainScheduler.instance)
             .bind(to: tableView.rx.items(cellIdentifier: "RecipeCell")) { (_, recipe: ModelRecipe, cell: RecipeCell) in
                 cell.setTitle(recipe.title)
                 cell.setIngredients(recipe.ingredients)
@@ -85,36 +85,10 @@ final class RecipesListView: UIViewController, ViewInterface {
 extension RecipesListView: RecipesListViewPresenterInterface {
     func showRecipes(recipes: Array<ModelRecipe>) {
         self.recipes.accept(recipes)
-        
-//        DispatchQueue.main.async {
-//            self.tableView.reloadData()
-//        }
     }
     
     func showError() {
 
-    }
-}
-
-extension RecipesListView: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipes.value.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        guard recipes.value.count > indexPath.row,
-            let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell") as? RecipeCell
-            else { return UITableViewCell() }
-
-        let recipe = recipes.value[indexPath.row]
-
-        cell.setTitle(recipe.title)
-        cell.setIngredients(recipe.ingredients)
-        cell.setHasLactose(recipe.hasLactose)
-        cell.setImage(recipe.image)
-
-        return cell
     }
 }
 
@@ -136,145 +110,8 @@ extension RecipesListView {
         tableView.rowHeight = UITableView.automaticDimension
         tableView.separatorStyle = .none
         tableView.accessibilityIdentifier = "RecipeTableView"
-        //tableView.dataSource = self
-        //        tableView.delegate = self
-        //        tableView.prefetchDataSource = self
         tableView.register(RecipeCell.self, forCellReuseIdentifier: "RecipeCell")
+
         return tableView
-    }
-}
-
-class RecipeCell: UITableViewCell {
-
-    private lazy var titleLabel: UILabel = self.createTitleLabel()
-    private lazy var ingredients: UILabel = self.createIngredientsLabel()
-    private lazy var hasLactose: UILabel = self.hasLactoseLabel()
-    private lazy var recipeImage: UIImageView = self.createRecipeImageView()
-    private lazy var makeFavorite: UIButton = self.createMakeFavoriteButton()
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-        backgroundColor = .clear
-        selectionStyle = .none
-
-        addSubview(recipeImage)
-        addSubview(makeFavorite)
-        addSubview(titleLabel)
-        addSubview(ingredients)
-        addSubview(hasLactose)
-
-        recipeImage.snp.makeConstraints { (make) in
-            make.width.equalToSuperview()
-            make.centerX.equalToSuperview()
-            make.topMargin.equalToSuperview()
-            make.height.equalTo(200)
-        }
-
-        makeFavorite.snp.makeConstraints { make in
-            make.top.equalTo(recipeImage.snp.bottom).offset(8)
-            make.right.equalToSuperview().inset(8)
-            make.width.equalTo(50)
-        }
-
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(recipeImage.snp.bottom).offset(8)
-            make.left.equalToSuperview().offset(8)
-            make.height.greaterThanOrEqualTo(44)
-            make.right.equalTo(makeFavorite.snp.left)
-        }
-
-        ingredients.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(8)
-            make.left.equalTo(titleLabel.snp.left)
-            make.right.equalTo(titleLabel.snp.right)
-            make.height.greaterThanOrEqualTo(44)
-            make.bottom.equalToSuperview().inset(8)
-        }
-
-        hasLactose.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(20)
-            make.right.equalToSuperview().offset(60)
-            make.height.equalTo(44)
-            make.width.equalTo(200)
-        }
-
-        clipsToBounds = true
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    func setTitle(_ title: String) {
-        self.titleLabel.text = title
-    }
-
-    func setIngredients(_ ingredients: String) {
-        self.ingredients.text = ingredients
-    }
-
-    func setFavorited(_ favorited: Bool) {
-        self.makeFavorite.isHidden = favorited
-    }
-
-    func setHasLactose(_ hasLactose: Bool) {
-        self.hasLactose.isHidden = !hasLactose
-    }
-
-    func setImage(_ image: URL?) {
-        self.recipeImage.kf.indicatorType = .activity
-        DispatchQueue.main.async {
-            self.recipeImage.kf.setImage(with: image!)
-        }
-
-    }
-}
-
-// MARK: Initialisers
-extension RecipeCell {
-    private func createRecipeImageView() -> UIImageView {
-        let imageView = UIImageView()
-        imageView.accessibilityIdentifier = "recipeImage"
-        imageView.backgroundColor = .clear
-        return imageView
-    }
-
-    private func createTitleLabel() -> UILabel {
-        let label = UILabel()
-        label.backgroundColor = .clear
-        label.accessibilityIdentifier = "title"
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 0
-        return label
-    }
-
-    private func createIngredientsLabel() -> UILabel {
-        let label = UILabel()
-        label.backgroundColor = .clear
-        label.accessibilityIdentifier = "ingredients"
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 0
-        return label
-    }
-
-    private func hasLactoseLabel() -> UILabel {
-        let label = UILabel()
-        label.backgroundColor = .red
-        label.textAlignment = .center
-        label.text = "Has Lactose"
-        label.accessibilityIdentifier = "lactose"
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 1
-
-        let radians = CGFloat.pi * CGFloat(45) / CGFloat(180.0)
-        label.transform = CGAffineTransform(rotationAngle: radians)
-        return label
-    }
-
-    private func createMakeFavoriteButton() -> UIButton {
-        let favorite = UIButton()
-        favorite.titleLabel?.text = "Make favorite"
-        return favorite
     }
 }

1e3cbaaf427415b386be484da00b48566578a18d
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2019-12-02 01:31:35 +0100

    Revert file to previous status

--- Recipes/Recipes/Services/Persistance/PersitanceServiceInterface.swift
+++ Recipes/Recipes/Services/Persistance/PersitanceServiceInterface.swift
@@ -18,20 +18,24 @@ extension PersitenceServiceInterface {
     private var persistenceThread: DispatchQueue { DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated) }
 
     func save(recipe: ModelRecipe, completionHandler: @escaping (Bool) -> Void) {
-        threadSafe(action: { self.save(recipe: recipe) }) { completionHandler($0) }
-    }
-
-    func remove(recipe: ModelRecipe, completionHandler: @escaping (Bool) -> Void) {
-        threadSafe(action: { self.remove(recipe: recipe) }) { completionHandler($0) }
-    }
-
-    func loadAll(completionHandler: @escaping ([ModelRecipe]) -> Void) {
-        threadSafe(action: { self.loadAll() }) { completionHandler($0) }
-    }
-
-    private func threadSafe<ResultType>(action: @escaping () -> ResultType, completionHandler: @escaping (ResultType) -> Void) {
         persistenceThread.async {
-            let result = action()
+            let result = self.save(recipe: recipe)
+            DispatchQueue.main.async {
+                completionHandler(result)
+            }
+        }
+    }
+    func remove(recipe: ModelRecipe, completionHandler: @escaping (Bool) -> Void) {
+        persistenceThread.async {
+            let result = self.remove(recipe: recipe)
+            DispatchQueue.main.async {
+                completionHandler(result)
+            }
+        }
+    }
+    func loadAll(completionHandler: @escaping ([ModelRecipe]) -> Void) {
+        persistenceThread.async {
+            let result = self.loadAll()
             DispatchQueue.main.async {
                 completionHandler(result)
             }
